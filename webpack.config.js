f725c5ae9b353d5ea6861cd5d48a9993bbe890ee
Edwin Guzman <edwin.gzmn@gmail.com>
2015-10-29 12:02:23 -0400

    Switching back to dist for entry file.

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,21 @@ var webpack = require('webpack');
 var cleanBuild = require('clean-webpack-plugin');
 
 module.exports = {
-  node: {
-    fs: "empty",
-    fsevents: "empty"
-  },
   context: __dirname + '/src',
   entry: './index.js',
   output: {
     filename: 'index.min.js',
     path: __dirname + '/dist'
   },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['babel']
+        loaders: ['babel-loader']
       }
     ]
   },
