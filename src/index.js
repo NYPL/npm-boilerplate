352c20ed1f5b41cbd17debeb6fde88d9f873d8fd
Edwin Guzman <edwin.gzmn@gmail.com>
2015-11-04 16:16:51 -0500

    Exporting an object instead of a function.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+var nyplModule;
+
 function staffPicksDate(date) {
   let d = date.match(/(\d{4})\-(\d{2})\-(\d{2})/),
     // year, month (0-11), day
@@ -13,4 +15,8 @@ function staffPicksDate(date) {
   }
 };
 
-module.exports = staffPicksDate;
+nyplModule = {
+  staffPicksDate: staffPicksDate
+};
+
+module.exports = nyplModule;
