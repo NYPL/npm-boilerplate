be55430b635d126daf81de9c09012ea8c07d001e
Edwin Guzman <edwin.gzmn@gmail.com>
2015-11-04 16:32:34 -0500

    Moving function call inside of object.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 var nyplModule;
 
-function staffPicksDate(date) {
-  let d = date.match(/(\d{4})\-(\d{2})\-(\d{2})/),
-    // year, month (0-11), day
-    newDate = new Date(d[1], d[2] - 1, d[3]),
-    monthsArr = ['January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'],
-    month = newDate.getMonth(),
-    year = newDate.getFullYear();
-
-  return {
-    month: monthsArr[month],
-    year
-  }
-};
-
 nyplModule = {
-  staffPicksDate: staffPicksDate
+  staffPicksDate: function staffPicksDate(date) {
+    var d = date.match(/(\d{4})\-(\d{2})\-(\d{2})/),
+      // year, month (0-11), day
+      newDate = new Date(d[1], d[2] - 1, d[3]),
+      monthsArr = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'],
+      month = newDate.getMonth(),
+      year = newDate.getFullYear();
+
+    return {
+      month: monthsArr[month],
+      year
+    };
+  }
 };
 
 module.exports = nyplModule;
